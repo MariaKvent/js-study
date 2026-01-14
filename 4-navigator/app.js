3c10181040ee4e7109a289dae601fe92181a29a6
Maria Kuzmicheva <mkventa@gmail.com>
2026-01-14 23:26:29 +0200

    Вносит правки

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -3,6 +3,6 @@ const addressLong = 50;
 const positionLat = 3;
 const positionLong = 40;
 
-const distanceBetweeen = ((((addressLat - positionLat) ** 2) + ((addressLong - positionLong) ** 2)) ** 0.5); // извлечение из корня заменила на возведение в степень 0.5
+const distanceBetweeen = (((addressLat - positionLat) ** 2) + ((addressLong - positionLong) ** 2)) ** 0.5; // извлечение из корня заменила на возведение в степень 0.5
 
 console.log(`Расстояние от меня до точки: ${distanceBetweeen} км`);
