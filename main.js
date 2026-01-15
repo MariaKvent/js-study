1360e52618ce31ca9eed7434fbc1e35864e0b1d1
Maria Kuzmicheva <mkventa@gmail.com>
2026-01-15 14:27:04 +0200

    5. switch-case: упражнение

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 1 упражнение
+
 const deposit = 12000;
 const currency = 'USD';
 const percent = 7;
@@ -10,4 +12,20 @@ if (result >= housePrice) {
 	console.log(`Мы накопили ${result} ${currency}. Хватит на дом. Еще останется ${result - housePrice} ${currency}`);
 } else {
 	console.log(`Мы накопили ${result} ${currency}. Нам не хватает ${housePrice - result} ${currency}`);
+}
+
+
+
+// 2 упражнение
+
+const message = prompt('Сколько будет 7 + или - 15?');
+
+switch(true) {
+	case Number(message) === (7 + 15):
+	case Number(message) === (7 - 15):
+	case message === 'Я не робот':
+		console.log('Ты не робот');
+		break;
+	default:
+		console.log('Держите робота!');
 }
