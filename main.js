6af7599d09478a16d825305b8af7b8211788a531
Maria Kuzmicheva <mkventa@gmail.com>
2026-01-19 17:40:20 +0200

    7. Функции - задачи

--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-const balance = 1000;
-const bonusBalance = 100;
-const isBanned = false;
-const isExist = false;
-const isSelling = true;
-const money = 1500;
-const bonusMoney = 50;
+// упражнение 1
 
-console.log(`Игра доступна для покупки: ${!isBanned && !isExist && isSelling && (money > balance || bonusMoney > bonusBalance)}`);
+// обычная функция
+function toPower(num, power) {
+	const res = num ** power;
+	return res;
+}
 
-console.log(!'' && 'Yes' || !('No' && 'false'));
+console.log(toPower(2, 3));
 
+// стрелочная функция
 
-const value = "test"; // Попробуйте изменить на разные значения: 0, null, undefined, "", "text", 42
+const toPowerArrow = (num, power) => num ** power;
 
-// Ваш код здесь
-const isTruthy = !!value;
-console.log(isTruthy);
+console.log(toPowerArrow(2, 5));
 
 
-const isWeekend = false;
-const isHoliday = false;
-const hasWork = true;
-const isUrgent = false;
+//===============
+// упражнение 2
 
-// Ваш код здесь
-const shouldWork = hasWork && (!isWeekend && !isHoliday || isUrgent);
-console.log(shouldWork);
+// дадут ли кредит
+
+const getCredit = (age, isWork = false) => {
+	switch (true) {
+		case (age >= 24 && isWork):
+			return 500;
+		case age >= 24: 
+			return 100;
+		default:
+			return 0;
+	}
+}
+
+const canBuy = (money, priceMac, age, isWork = false) => {
+	if (money < priceMac) {
+		return money + getCredit(age, isWork) >= priceMac;
+	}
+	return true;
+}
+
+console.log(canBuy(1500, 2000, 24, true));
+
+
+// вариант от ИИ
+const canBuy2 = (price, money, age, isWork = false) => {
+	let credit = 0;
+
+	if (age >= 24 && isWork) {
+		credit = 500;
+	} else if (age >= 24) {
+		credit = 100;
+	}
+
+	return money + credit >= price;
+};
+
+console.log(canBuy2(1500, 2000, 24, true));
+
+
+
+//
+function getUser(id = 1, name) {
+    console.log(id + ' ' + name);
+}
+getUser('Антон');
+
+
+const getLength = (str) => str.length;
+console.log(getLength('Привет'));
+
+
+const sum = (a, b) => a + b;
+function printSum(a, b) {
+	return console.log(`Значение суммы ${a} и ${b} равно ${sum(a, b)}`);
+}
+printSum(2, 3);
